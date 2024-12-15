92906779a01a15d5dee0c6977b8c81210e040554
Svdex <renixiel@gmail.com>
2024-12-16 03:18:47 +0700

    fix: add setvariable animation overriding

--- src/utils/variable.ts
+++ src/utils/variable.ts
@@ -1,10 +1,24 @@
 import { Entity, system } from "@minecraft/server";
 
+export type SetVariableOptions = {
+  /**
+   * The first dummy animation to play.
+   * @default "animation.player.attack.positions"
+   */
+  animation1?: string;
+  /**
+   * The second dummy animation to play.
+   * @default "animation.humanoid.base_pose"
+   */
+  animation2?: string;
+};
+
 /**
  * Set a variable to be used in client-side entities
  * @param entity - The entity to set the variable on
  * @param key - The key of the variable
  * @param value - The value of the variable
+ * @param options - The options for setting the variable
  * @example
  * ```ts
  * setVariable(entity, "health", 20); // Equivalent to v.health = 20;
@@ -20,17 +34,26 @@ import { Entity, system } from "@minecraft/server";
  * }
  * ```
  */
-export async function setVariable(entity: Entity, key: string, value: string | number) {
+export async function setVariable(
+  entity: Entity,
+  key: string,
+  value: string | number,
+  options?: SetVariableOptions,
+) {
+  const {
+    animation1 = "animation.player.attack.positions",
+    animation2 = "animation.humanoid.base_pose",
+  } = options ?? {};
   const controller = `${key}.${value}`;
   if (typeof value === "string") {
     value = `'${value}'`;
   }
-  entity.playAnimation("animation.player.attack.positions", {
+  entity.playAnimation(animation1, {
     controller,
     stopExpression: `v.${key} = ${value}; return 1;`,
   });
   await system.waitTicks(1);
-  entity.playAnimation("animation.humanoid.base_pose", {
+  entity.playAnimation(animation2, {
     controller,
     stopExpression: "0",
   });
