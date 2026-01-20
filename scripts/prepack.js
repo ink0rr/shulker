e636079c786ac69f7a2c888faa6fcbac0e17c390
Beltsazar <tomflynnbeltsazar@gmail.com>
2026-01-20 13:18:10 +0700

    fix: missing type declarations

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -1,5 +1,4 @@
 const path = "./dist/index.d.mts";
-const text = `/// <reference types="../server.d.mts" />
-/// <reference types="../server-ui.d.mts" />
+const text = `/// <reference types="../server-ui.d.mts" />
 ${await Bun.file(path).text()}`;
 await Bun.write(path, text);
