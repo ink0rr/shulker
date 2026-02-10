787472096d628e4cacf781ef7c0ec12d9434d8ab
Svdex <renixiel@gmail.com>
2026-02-10 16:03:57 +0700

    refactor: change formatter class to function

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,162 +1,90 @@
 import { world } from "@minecraft/server";
 
-class ShulkerFormatter {
-  protected colors = {
-    string: "§a",
-    number: "§6",
-    null: "§8",
-    boolean: "§9",
-    undefined: "§8",
-    key: "§7",
-    reset: "§r",
-    colon: "§7",
-    brackets: ["§e", "§d", "§9"],
-  };
-  protected skipProperties = new Set([
-    "constructor",
-    "__defineGetter__",
-    "__defineSetter__",
-    "__lookupGetter__",
-    "__lookupSetter__",
-    "hasOwnProperty",
-    "isPrototypeOf",
-    "propertyIsEnumerable",
-    "toString",
-    "valueOf",
-    "__proto__",
-    "toLocaleString",
-  ]);
-  protected escape(string: string): string {
-    return string
-      .replace(/\\/g, "\\\\")
-      .replace(/"/g, '\\"')
-      .replace(/\n/g, "\\n")
-      .replace(/\r/g, "\\r")
-      .replace(/\t/g, "\\t");
+const colors = {
+  string: "§a",
+  number: "§6",
+  null: "§8",
+  boolean: "§9",
+  undefined: "§8",
+  key: "§7",
+  reset: "§r",
+  colon: "§7",
+  brackets: ["§e", "§d", "§9"],
+} as const;
+type ColorType = keyof typeof colors;
+const skipProperties = new Set([
+  "constructor",
+  "__defineGetter__",
+  "__defineSetter__",
+  "__lookupGetter__",
+  "__lookupSetter__",
+  "hasOwnProperty",
+  "isPrototypeOf",
+  "propertyIsEnumerable",
+  "toString",
+  "valueOf",
+  "__proto__",
+  "toLocaleString",
+]);
+
+function escape(string: string): string {
+  return string
+    .replace(/\\/g, "\\\\")
+    .replace(/"/g, '\\"')
+    .replace(/\n/g, "\\n")
+    .replace(/\r/g, "\\r")
+    .replace(/\t/g, "\\t");
+}
+
+function newLine(indent: number): string {
+  return "\n" + "  ".repeat(indent);
+}
+
+type WrapBracketsOptions = {
+  type: "array" | "object";
+  bracketIndex: number;
+  colored?: boolean;
+  str: string;
+};
+function wrapBrackets(options: WrapBracketsOptions): string {
+  const { type, bracketIndex, colored, str } = options;
+  const brackets = colors.brackets;
+  const reset = colors.reset;
+  const color = colored ? brackets[bracketIndex % brackets.length] : "";
+  if (type === "array") {
+    return color + "[" + str + color + "]" + reset;
+  } else {
+    return color + "{" + str + color + "}" + reset;
   }
-  protected newLine(indent: number): string {
-    return "\n" + "  ".repeat(indent);
+}
+
+type ColorizeOptions = {
+  type: ColorType;
+  str: string;
+  colored?: boolean;
+};
+function colorize(options: ColorizeOptions): string {
+  const { type, str, colored } = options;
+  if (!colored) {
+    return str;
   }
-  protected compileArray(o: unknown[], options: FormatOptions = { indent: 1 }): string {
-    const { indent, bracketIndex = 0, breaks = false, colored = true } = options;
-    const hasObj = o.some((v) => typeof v === "object" && v !== null);
-    const color = colored ? this.colors.brackets[bracketIndex % this.colors.brackets.length] : "";
-    if (hasObj || breaks) {
-      // Newlines
-      return (
-        color +
-        "[" +
-        this.newLine(indent + 1) +
-        o
-          .map((v) =>
-            this.format(v, {
-              indent: typeof v === "object" ? indent + 2 : indent,
-              breaks: true,
-              bracketIndex: bracketIndex + 1,
-              colored,
-              quoteString: true,
-            }),
-          )
-          .join(`${this.colors.reset}, ` + this.newLine(indent + 1)) +
-        this.newLine(indent) +
-        color +
-        "]" +
-        this.colors.reset
-      );
-    } else {
-      // No newlines
-      return (
-        color +
-        "[" +
-        o
-          .map((v) =>
-            this.format(v, {
-              indent,
-              bracketIndex: bracketIndex + 1,
-              colored,
-              quoteString: true,
-            }),
-          )
-          .join(`${this.colors.reset}, `) +
-        color +
-        "]" +
-        this.colors.reset
-      );
+  const color = colors[type];
+  const reset = colors.reset;
+  return color + str + reset;
+}
+
+function* iterateKeys(o: object): Iterable<string> {
+  const proto = Object.getPrototypeOf(o);
+  if (proto === null) {
+    return;
+  }
+  const keys = proto === Object.prototype
+    ? Object.getOwnPropertyNames(o)
+    : Object.getOwnPropertyNames(proto);
+  for (const key of keys) {
+    if (!skipProperties.has(key)) {
+      yield key;
     }
-  }
-  format(o: unknown, options: FormatOptions = { indent: 1 }): string {
-    const { indent, breaks = true, bracketIndex = 0, colored = true } = options;
-    let out = "";
-    if (Array.isArray(o)) {
-      // Array
-      out += this.compileArray(o, { ...options, indent });
-    } else if (typeof o === "object" && o !== null) {
-      // Object
-      let hasContent = false;
-      const color = colored ? this.colors.brackets[bracketIndex % this.colors.brackets.length] : "";
-      out += `${color}{${this.colors.reset}`;
-      const keys = Object.keys(o).concat(Object.getOwnPropertyNames(Object.getPrototypeOf(o)));
-      for (const key of keys) {
-        if (this.skipProperties.has(key)) {
-          continue;
-        }
-        const compiled = this.format(o[key as keyof typeof o], {
-          indent: indent + 1,
-          breaks,
-          bracketIndex: bracketIndex + 1,
-          colored,
-          quoteString: true,
-        });
-        if (compiled) {
-          if (hasContent) {
-            if (breaks) {
-              out += ",";
-            } else {
-              out += ", ";
-            }
-          }
-          if (breaks) {
-            out += this.newLine(indent);
-          }
-          const str = this.escape(key);
-          out += `${this.colors.key}${str}${this.colors.colon}:${this.colors.reset} ${compiled}`;
-          hasContent = true;
-        }
-      }
-      if (hasContent && breaks) {
-        out += this.newLine(indent - 1);
-      }
-      out += `${color}}${this.colors.reset}`;
-    } else if (o === null) {
-      // Null
-      const color = colored ? this.colors.null : "";
-      out += `${color}null${this.colors.reset}`;
-    } else if (typeof o === "string") {
-      // String
-      const color = colored ? this.colors.string : "";
-      let str = this.escape(o);
-      if (options.quoteString) {
-        str = `${color}"${str}"`;
-      }
-      out += `${str}${this.colors.reset}`;
-    } else if (typeof o === "number") {
-      // Number
-      const color = colored ? this.colors.number : "";
-      out += `${color}${o}${this.colors.reset}`;
-    } else if (typeof o === "boolean") {
-      // Boolean
-      const color = colored ? this.colors.boolean : "";
-      out += `${color}${o}${this.colors.reset}`;
-    } else if (typeof o === "undefined") {
-      // Undefined
-      const color = colored ? this.colors.undefined : "";
-      out += `${color}undefined${this.colors.reset}`;
-    } else if (typeof o === "bigint") {
-      // BigInt
-      const color = colored ? this.colors.number : "";
-      out += `${color}${o}n${this.colors.reset}`;
-    }
-    return out;
   }
 }
 
@@ -167,8 +95,106 @@ type FormatOptions = {
   colored?: boolean;
   quoteString?: boolean;
 };
-
-const formatter = new ShulkerFormatter();
+function formatArray(o: unknown[], options: FormatOptions): string {
+  const { indent, breaks = true, bracketIndex = 0, colored = true } = options;
+  let str = "";
+  const innerOptions = {
+    ...options,
+    indent: indent + 1,
+    bracketIndex: bracketIndex + 1,
+  } satisfies FormatOptions;
+  const n1 = newLine(indent + 1);
+  for (let i = 0; i < o.length; i++) {
+    const item = o[i];
+    const compiled = format(item, innerOptions);
+    if (breaks) {
+      str += n1;
+    }
+    str += compiled;
+    if (i < o.length - 1) {
+      str += ",";
+    }
+  }
+  if (breaks && str) {
+    str += newLine(indent);
+  }
+  return wrapBrackets({
+    type: "array",
+    bracketIndex,
+    colored,
+    str,
+  });
+}
+export function format(o: unknown, options: FormatOptions = { indent: 0 }): string {
+  const { indent, breaks = true, bracketIndex = 0, colored = true, quoteString = true } = options;
+  switch (typeof o) {
+    case "string": {
+      let str = escape(o);
+      if (quoteString || !str) {
+        str = `"${str}"`;
+      }
+      return colorize({ type: "string", str, colored });
+    }
+    case "number": {
+      return colorize({ type: "number", str: o.toString(), colored });
+    }
+    case "boolean": {
+      return colorize({ type: "boolean", str: o.toString(), colored });
+    }
+    case "bigint": {
+      return colorize({ type: "number", str: o.toString() + "n", colored });
+    }
+    case "undefined": {
+      return colorize({ type: "undefined", str: "undefined", colored });
+    }
+    case "object": {
+      if (o === null) {
+        return colorize({ type: "null", str: "null", colored });
+      }
+      if (Array.isArray(o)) {
+        return formatArray(o, options);
+      }
+      let out = "";
+      let hasContent = false;
+      const innerOptions = {
+        ...options,
+        indent: indent + 1,
+        bracketIndex: bracketIndex + 1,
+      } satisfies FormatOptions;
+      const n1 = newLine(indent + 1);
+      for (const key of iterateKeys(o)) {
+        const compiled = format(o[key as keyof typeof o], innerOptions);
+        if (compiled) {
+          if (hasContent) {
+            if (breaks) {
+              out += ",";
+            } else {
+              out += ", ";
+            }
+          }
+          if (breaks) {
+            out += n1;
+          }
+          const str = escape(key);
+          const sKey = colorize({ type: "key", str, colored });
+          const sColon = colorize({ type: "colon", str: ":", colored });
+          out += `${sKey}${sColon} ${compiled}`;
+          hasContent = true;
+        }
+      }
+      if (hasContent && breaks) {
+        out += newLine(indent);
+      }
+      return wrapBrackets({
+        type: "object",
+        bracketIndex,
+        colored,
+        str: out,
+      });
+    }
+  }
+  return "";
+}
 
 /**
  * A simple debugging utility for logging messages to the Minecraft chat.
@@ -209,6 +235,6 @@ export const debug = Object.freeze({
 });
 
 function impl(method: string, ...args: unknown[]) {
-  const formatted = args.map((v) => formatter.format(v)).join(" ");
+  const formatted = args.map((v) => format(v)).join(" ");
   world.sendMessage(`[${method}] ${formatted}`);
 }
